cfaa374085e5a73e2a7b649e76db35dfc5386dc6
pavansw <pavanwankhade4u@gmail.com>
2022-01-11 14:35:20 +0530

    Helllo updated for demo

--- hello.py
+++ hello.py
@@ -2,3 +2,15 @@ print('Hello all , Welcome to Devops')
 a,b=10,20
 print(a+b)
 # comment 1
+print('Hello all , Welcome to Devops')
+print('Hello all , Welcome to Devops')
+print('Hello all , Welcome to Devops')
+print('Hello all , Welcome to Devops')
+a,b=10,20
+print(a+b)
+a,b=10,20
+print(a+b)
+a,b=10,20
+print(a+b)
+a,b=10,20
+print(a+b)
